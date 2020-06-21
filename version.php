65a01aefe4c778d76455381d9a38761904e3c35e
valery <valery.fremaux@gmail.com>
2020-06-21 10:40:25 +0200

    Automated sync from SVN

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2017030700;
-$plugin->requires  = 2018112800;
+$plugin->requires  = 2019051100;
 $plugin->component = 'quizaccess_usernumattempts';
-$plugin->release = '3.6.0 (Build 2017030700)';
+$plugin->release = '3.7.0 (Build 2017030700)';
 $plugin->maturity = MATURITY_STABLE;
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.6.0001';
+$plugin->codeincrement = '3.7.0002';
